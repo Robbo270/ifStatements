9e020849b6a0de0cc2f1c0ea4f028179e208e1ab
Robbo270 <116882868+Robbo270@users.noreply.github.com>
2022-11-11 13:02:09 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,32 @@
+
 function setup() {
     var canvas = createCanvas(800, 800);
-    canvas.parent("p5container");
+    canvas.parent("p5container");        
 }
 
 function draw() {
     background(220);
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    if(mouseX < width/2){
+        console.log('Smile - Left'); //write something in console
+        circle(400,400,400);
+        fill(255,204,0);
+        circle(300,330,100);
+        circle(500,330,100);
+        
+        
+    }
+
+    if(mouseX > width/2){
+        console.log('Frown - Right'); //write something in console
+        circle(400,400,400);
+        fill(255,0,0);
+        circle(300,330,100);
+        circle(500,330,100);
+
+    }
+    console.log('LEFT'); //write something in console
+    console.log('RIGHT'); //write something in console
+
+// Simplify the Code and write "else" - not always used but can shorten code in future
 }
